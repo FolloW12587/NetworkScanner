985f83c07b766d98520b63502970592b5aab765d
follow <follow12587@gmail.com>
2024-03-24 09:56:29 +1000

    Initial Commit

---
+++ NetworkScanner/NetworkScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkScannerApp.swift
+//  NetworkScanner
+//
+//  Created by Сергей Дубовой on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
